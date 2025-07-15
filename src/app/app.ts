5580a564639e589ef24521e3a7fdc706de912971
pelidigatto <florianthoenelt@outlook.de>
2025-07-15 06:24:01 +0200

    Revert "[F] Install PrimeNg"
    This reverts commit 11aa66f7712cfb3d2581baae53071400eb55fea1.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
