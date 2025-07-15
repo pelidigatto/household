4312686db7332f592fadfdce5625d91fa0778cae
pelidigatto <florianthoenelt@outlook.de>
2025-07-15 06:26:17 +0200

    [F] Introduce Nav

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navigation} from './navigation/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
